39164552f3bc9d3cba80538a1e0bb929d9c37229
Sumi <qkobr94@gmail.com>
2025-11-16 20:52:58 +0100

    feat/create-detail-page

--- app/(dashboard)/recipes/[id].tsx
+++ app/(dashboard)/recipes/[id].tsx
@@ -25,17 +25,43 @@ const RecipeDetail = () => {
   const recipeInfo = data?.recipes_by_pk;
   if (!recipeInfo) return <ThemedText>Recette non trouvée</ThemedText>;
 
+  const chunkRecipe = (recipe: string | null) => {
+    const ingredients = recipe?.indexOf("1");
+    const ingredientsTitle = "🧑🏻‍🍳 Recette & Ingredients";
+    const rest = ingredients && recipe?.slice(ingredients);
+    const splitted = rest ? rest.split(". ") : [];
+    const formatted = splitted && splitted.map((line) => line + "\n").join("");
+
+    return (
+      <>
+        <ThemedText title style={{ marginBottom: 5, fontSize: 16 }}>
+          {ingredientsTitle}
+        </ThemedText>
+        {formatted.split("\n").map((format) => {
+          if (isNaN(parseInt(format)) && format !== "")
+            return <ThemedText key={format}>* {format}.</ThemedText>;
+        })}
+      </>
+    );
+  };
   return (
     <ThemedView safe={true}>
-      <ThemedText title>Recette & Ingredients</ThemedText>
-      <ThemedCard style={{margin:10, padding:10 , justifyContent:"center"}}>
+      <ThemedCard style={{ margin: 10, padding: 10, justifyContent: "center" }}>
         <Image
           source={{
             uri: `https://media.aboutsauce.com/${recipeInfo.id}.jpg?quality=100`,
           }}
-          style={{ width:300, height: 300, borderRadius: 10 }}
+          style={{ width: 300, height: 300, borderRadius: 10 }}
         />
-        <ThemedText style={{marginVertical:20}}title>{recipeInfo.name}</ThemedText>
+        <ThemedText style={{ marginVertical: 10, fontSize: 18 }} title>
+          {recipeInfo.name}
+        </ThemedText>
+        <ThemedText
+          style={{ fontSize: 14, fontWeight: "bold", marginBottom: 10 }}
+        >
+          {recipeInfo.description}
+        </ThemedText>
+        {chunkRecipe(recipeInfo.recipe)}
       </ThemedCard>
     </ThemedView>
   );
