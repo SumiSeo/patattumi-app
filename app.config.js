694e07f4a4f9427132abebb36a1d9d52ea4b7ac5
Sumi <qkobr94@gmail.com>
2025-11-17 14:33:38 +0100

    feat/change-settinogs

--- app.config.js
+++ app.config.js
@@ -2,6 +2,22 @@ import 'dotenv/config';
 
 export default {
   expo: {
+    name: "patattumi",
+    slug: "patattumi",
+    version: "1.0.0",
+    ios: {
+      bundleIdentifier: "com.anonymous.patattumiapp",
+      supportsTablet: true,
+      usesAppleSignIn: true,
+    },
+    android: {
+      package: "com.anonymous.patattumiapp",
+      adaptiveIcon: {
+        foregroundImage: "./assets/icons/adaptive-icon.png",
+        monochromeImage: "./assets/icons/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
     extra: {
       HASURA_SECRET: process.env.EXPO_PUBLIC_HASURA_ADMIN_SECRET,
       HASURA_URI: process.env.EXPO_PUBLIC_HASURA_URI,
