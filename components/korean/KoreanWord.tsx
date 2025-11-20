a3f42d268aa4f966d70217b9bf0d39e11748b95b
Sumi <qkobr94@gmail.com>
2025-11-20 12:16:53 +0100

    feat/create-table

--- components/korean/KoreanWord.tsx
+++ components/korean/KoreanWord.tsx
@@ -1,8 +1,18 @@
 import { wordsData } from "@/types/KoreanClockGameType";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const KoreanWord = ({ data }: wordsData) => {
+  const [showWords, setShowWords] = useState<boolean>(true);
+  const [showSignications, setShowSignifications] = useState<boolean>(true);
+  const handlePressWords = () => {
+    setShowWords(!showWords);
+  };
+  const handlePressSignifications = () => {
+    setShowSignifications(!showSignications);
+  };
+
   const createWordsTable = () => {
     return data.map((word) => (
       <View
@@ -11,11 +21,25 @@ const KoreanWord = ({ data }: wordsData) => {
           flexDirection: "row",
           paddingVertical: 8,
           borderBottomWidth: 0.2,
-          borderColor: "black"
+          borderColor: "black",
         }}
       >
-        <Text style={[{ flex: 1 }, styles.tableRow]}>{word.korean}</Text>
-        <Text style={[{ flex: 2 }, styles.tableRow]}>{word.signification}</Text>
+        <Text
+          style={[
+            { flex: 1, color: !showWords ? "white" : "black" },
+            styles.tableRow,
+          ]}
+        >
+          {word.korean}
+        </Text>
+        <Text
+          style={[
+            { flex: 2, color: !showSignications ? "white" : "black" },
+            styles.tableRow,
+          ]}
+        >
+          {word.signification}
+        </Text>
       </View>
     ));
   };
@@ -25,13 +49,46 @@ const KoreanWord = ({ data }: wordsData) => {
       <View
         style={{
           flexDirection: "row",
-          paddingVertical: 8,
+          paddingVertical: 10,
           backgroundColor: "black",
           borderBottomWidth: 2,
         }}
       >
-        <Text style={[{ flex: 1 }, styles.tableHeader]}>Mot</Text>
-        <Text style={[{ flex: 2 }, styles.tableHeader]}>Signification</Text>
+        <Pressable
+          onPress={handlePressWords}
+          style={[
+            {
+              flex: 1,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "flex-start",
+            },
+          ]}
+        >
+          <Ionicons
+            size={14}
+            name={showWords ? "eye-outline" : "eye-off-outline"}
+            color="white"
+          />
+          <Text style={styles.tableHeader}>Mot</Text>
+        </Pressable>
+        <Pressable
+          onPress={handlePressSignifications}
+          style={[
+            {
+              flex: 2,
+              flexDirection: "row",
+              alignItems: "center",
+            },
+          ]}
+        >
+          <Ionicons
+            size={14}
+            name={showSignications ? "eye-outline" : "eye-off-outline"}
+            color="white"
+          />
+          <Text style={styles.tableHeader}>Signification</Text>
+        </Pressable>
       </View>
 
       {createWordsTable()}
@@ -43,10 +100,10 @@ export default KoreanWord;
 
 const styles = StyleSheet.create({
   tableHeader: {
-    textAlign: "center",
     fontWeight: "bold",
     color: "white",
     fontSize: 12,
+    marginLeft: 5,
   },
   tableRow: {
     fontSize: 10,
