7f3872bcd62f7060cc9260d42f24e4cca613d788
Sumi <qkobr94@gmail.com>
2025-11-20 15:00:31 +0100

    feat/change-ui-for-calnedar

--- components/culture/KoreanHolidayCalendar.tsx
+++ components/culture/KoreanHolidayCalendar.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { View } from 'react-native'
-import ThemedCalendar from '../DateTime/ThemedCalendar'
+import React from "react";
+import ThemedCalendar from "../DateTime/ThemedCalendar";
+import Spacer from "../Spacer";
+import ThemedText from "../ThemedText";
 
 const KoreanHolidayCalendar = () => {
   return (
-    <View>
-    <ThemedCalendar/>
-    </View>
-  )
-}
+    <>
+      <ThemedText style={{ marginTop: 10 }}>
+        En Corée, les horaires d&apos;ouverture des musées, des trains ou
+        d&apos;autres services peuvent changer pendant les jours fériés. Pensez
+        donc à vérifier avant votre voyage.
+      </ThemedText>
+      <Spacer height={5} />
+      <ThemedText>
+        Sachez aussi que pendant les grandes fêtes traditionnelles comme Seollal
+        ou Chuseok, de nombreuses boutiques peuvent être fermées.
+      </ThemedText>
+      <Spacer height={10} />
+      <ThemedCalendar />
+    </>
+  );
+};
 
-export default KoreanHolidayCalendar
+export default KoreanHolidayCalendar;
